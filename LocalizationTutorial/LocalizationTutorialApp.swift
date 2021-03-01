cf7f1d36dead829c503eb8f57734fcd402232d8c
Elechim01 <mikymanny015@gmail.com>
2021-03-01 14:52:38 +0100

    Initial Commit

---
+++ LocalizationTutorial/LocalizationTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationTutorialApp.swift
+//  LocalizationTutorial
+//
+//  Created by Michele Manniello on 01/03/21.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
